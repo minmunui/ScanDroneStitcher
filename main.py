34c5a6d673344e40f91abe48572f656c2113f813
DongjinPark <ehdwls1638@pusan.ac.kr>
2024-09-23 22:19:19 +0900

    feat : add pointing on pano

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ from tqdm import tqdm
 
 from src.gps import get_gps_from_image, get_angles, determine_rotation_angles
 from src.img_io import make_output_name
+from template_match import search_locations
 
 DISCARDED = -1
 ORIGINAL = 0
@@ -26,6 +27,7 @@ def main():
     parser.add_argument('-l', '--log', type=str, help='is remain log at console', nargs='?', default="log")
     parser.add_argument('-r', '--resize', type=int, help='resize images', nargs='?', default=1)
     parser.add_argument('-p', '--pano_conf', type=float, help='panorama confidence', nargs='?', default=1.0)
+    parser.add_argument('-d', '--drop', type=int, help='ratio of drop', nargs='?', default=1)
     args = parser.parse_args()
 
     input_dir = args.input  # 입력 디렉토리 이름
@@ -39,7 +41,7 @@ def main():
 
     coordinates = []
 
-    for image_path in tqdm(image_paths, desc="reading images"):
+    for image_path in tqdm(image_paths, desc="extract gps"):
         coordinates.append(get_gps_from_image(img_path=image_path)[:2])
 
     angles = get_angles(coordinates)
@@ -75,6 +77,8 @@ def main():
             image = cv2.rotate(image, cv2.ROTATE_180)
         images.append(image)
 
+
+
     output_dir = make_output_name()
 
     if args.output != "result":
@@ -82,8 +86,11 @@ def main():
     output_base = os.path.join(os.getcwd(), "data", "output", output_dir)
     os.makedirs(output_base, exist_ok=True)
 
-    print(f"len(images) : {len(images)}")
-    print(f"len(image_names) : {len(image_names)}")
+    if args.drop != 1:
+        images = [images[i] for i in range(len(images)) if i % args.drop == 0]
+        image_names = [image_names[i] for i in range(len(image_names)) if i % args.drop == 0]
+        coordinates = [coordinates[i] for i in range(len(coordinates)) if i % args.drop == 0]
+
     for idx, image_name in enumerate(image_names):
         cv2.imwrite(os.path.join(os.getcwd(), "data", "output", output_dir, f"{idx}_{image_name}"), images[idx])
 
@@ -92,7 +99,6 @@ def main():
     stitcher = cv2.Stitcher.create(mode=cv2.STITCHER_SCANS)
     stitcher.setPanoConfidenceThresh(args.pano_conf)
 
-    print(f"resize images : {args.resize}")
     if args.resize != 1:
         for i in range(len(images)):
             images[i] = cv2.resize(images[i], (images[i].shape[1] // args.resize, images[i].shape[0] // args.resize))
@@ -101,7 +107,20 @@ def main():
 
     if status == cv2.Stitcher_OK:
         print("Stitching successful.")
+        print("find points")
         cv2.imwrite(os.path.join(output_base, "stitched_output.jpg"), stitched)
+        points, pointed_image = search_locations(images, stitched)
+
+        point_coordinates = []
+        with open(os.path.join(output_base, "pointed_output.txt"), 'w') as f:
+            for i in range(len(points)):
+                if points[i] is not None:
+                    point_coordinates.append((points[i], coordinates[i]))
+                    f.write(f"{point_coordinates[-1]}\n")
+                    cv2.circle(stitched, points[i], 5, 255, -1)
+                    cv2.putText(stitched, f"{i}", points[i], cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2)
+        cv2.imwrite(os.path.join(output_base, "pointed_output.jpg"), stitched)
+        print("Point Coordinates: ")
     else:
         print("Stitching failed. Error code: ", status)
 
